f011c64653d9e6ad9daa9355c6f8fa357069351c
AkashSingh2218271 <akrawat0056@gmail.com>
2025-03-05 21:24:43 +0530

    adding new file neighborhoods.cpp

---
+++ codechef_100_900_/Neighborhoods.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    
+    const int total_neighborhoods = 100;
+    int remaining = total_neighborhoods - N;
+    cout << remaining << endl;
+    
+    return 0;
+}
