2bad34e4c53b0f436c38d296fb7bf0851d4f3a11
AkashSingh2218271 <akrawat0056@gmail.com>
2025-03-06 21:26:30 +0530

    adding file Oct_marthon.cpp

---
+++ codechef_100_900_/Oct_Marthon.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int x;
+    cin >> x;
+
+    if (x < 3) {
+        cout << "gold\n";
+    } else if (x >= 3 && x < 6) {
+        cout << "silver\n";
+    } else {
+        cout << "bronze\n";
+    }
+    
+    return 0;
+}
