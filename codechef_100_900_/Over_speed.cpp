f260a0b4acfcf5c1b01ec71c7ef8618c73c4b277
AkashSingh2218271 <akrawat0056@gmail.com>
2025-03-10 21:18:59 +0530

    adding file over_speed.cpp

---
+++ codechef_100_900_/Over_speed.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    
+    while (t--) {
+        int speed;
+	    cin >> speed;
+	
+    	if (speed <= 70) {
+    	  cout << 0 << endl;  
+    	} else if (speed > 70 && speed <= 100) {
+    	    cout << 500 << endl;
+    	} else {
+    	    cout << 2000 << endl;
+    	}
+    }
+	
+    return 0;
+}
