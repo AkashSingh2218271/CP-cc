7126d8a4989210fb7705f15d77fb2e7c31a71dfd
AkashSingh2218271 <akrawat0056@gmail.com>
2025-03-13 13:41:14 +0530

    adding file podium_finish.cpp

---
+++ codechef_100_900_/Podium_finish.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    
+    while (t--) {
+        int gap1, gap2;
+        cin >> gap1 >> gap2;
+        
+        cout << gap1 + gap2 << endl;
+    }
+    return 0;
+}
