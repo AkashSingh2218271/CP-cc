f91a9a85a05fd0d26aeafaec3ca810e3c7880ea4
AkashSingh2218271 <akrawat0056@gmail.com>
2025-03-16 23:53:16 +0530

    adding file aqi.cpp

---
+++ codechef_100_900_/aqi.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int aqi;
+    cin >> aqi;
+    
+    if (aqi < 100) {
+        cout << "yes\n";
+    } else {
+        cout << "no\n";
+    }
+    
+    return 0;
+}
