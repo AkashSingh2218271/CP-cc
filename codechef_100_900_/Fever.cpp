03d5e5c39cddb16afe8eabdd7ba3f73527bba054
AkashSingh2218271 <akrawat0056@gmail.com>
2025-03-17 22:07:37 +0530

    adding file fever.cpp

---
+++ codechef_100_900_/Fever.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    
+    while (t--) {
+        int temp;
+        cin >> temp;
+        
+        if (temp > 98) {
+            cout << "yes\n";
+        } else {
+            cout << "no\n";
+        }
+    }
+    
+    return 0;
+}
