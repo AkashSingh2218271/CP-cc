67699a55ada20090a0f5c45b96acdb4eec6510b2
AkashSingh2218271 <akrawat0056@gmail.com>
2025-03-18 23:51:33 +0530

    adding file sleep.cpp

---
+++ codechef_100_900_/Sleep_deprevation.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	
+	while (t--) {
+	    int hrs;
+	    cin >> hrs;
+	    
+	    if (hrs < 7) {
+	        cout << "yes" << endl;
+	    } else {
+	        cout << "no" << endl;
+	    }
+	}
+	
+    return 0;
+}
