282bc48bbc3b7cbb3c5c8de7a7d2b77e11b6a102
AkashSingh2218271 <akrawat0056@gmail.com>
2025-03-21 22:39:37 +0530

    adding code wr.cpp

---
+++ codechef_100_900_/Water_requirement.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    
+    while (t--) {
+        int hrs;
+        cin >> hrs;
+        
+        hrs *= 2;
+        
+        cout << hrs << endl;
+    }
+    
+    return 0;
+}
