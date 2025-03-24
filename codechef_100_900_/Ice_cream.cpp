00d26cc3a130a86d399b25391bff82f67344849a
AkashSingh2218271 <akrawat0056@gmail.com>
2025-03-24 21:50:10 +0530

    adding file ic.cpp

---
+++ codechef_100_900_/Ice_cream.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int cost1, y;
+    cin >> cost1 >> y;
+    
+    if ((cost1 + cost1) > y) {
+        cout << "no\n";
+    } else {
+        cout << "yes\n";
+    }
+    
+    return 0;
+}
