62fa4df29ae30619c15a8d9fbcc26c51e45d52bc
AkashSingh2218271 <akrawat0056@gmail.com>
2025-03-25 23:48:26 +0530

    adding file Aao.cpp

---
+++ codechef_100_900_/Apples_and_oranges.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int x, a, b;
+    cin >> x >> a >> b;
+    
+    if ((a + b) <= x) {
+        cout << "yes\n";
+    } else {
+        cout << "no\n";    
+    }
+    
+    return 0;
+}
