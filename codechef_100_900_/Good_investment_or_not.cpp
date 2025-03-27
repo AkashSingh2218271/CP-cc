df5cd2c61472af6a99464b41ca4bbeb1060cf4c6
AkashSingh2218271 <akrawat0056@gmail.com>
2025-03-27 18:52:42 +0530

    adding file gion.cpp

---
+++ codechef_100_900_/Good_investment_or_not.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    
+    while (t--) {
+        int x, y;
+        cin >> x >> y;
+        
+        if (x >= (2 * y)) {
+            cout << "yes\n";  
+        } else {
+            cout << "no\n";
+        }
+    }
+    
+	return 0;
+}
