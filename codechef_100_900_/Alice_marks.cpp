3abc47d788d0d127fe7a64dc6f210395ef6b0d65
AkashSingh2218271 <akrawat0056@gmail.com>
2025-03-29 22:50:03 +0530

    adding file am.cpp

---
+++ codechef_100_900_/Alice_marks.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x,y;
+    cin>>x>>y;
+	
+	if (x >= (2 * y)) {
+	    cout << "yes\n";
+	} else {
+	    cout << "no\n";
+	}
+	
+	return 0;
+}
