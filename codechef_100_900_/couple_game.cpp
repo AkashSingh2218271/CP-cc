5e76b19048e779749f9d86f8ed4db99297a251be
AkashSingh2218271 <akrawat0056@gmail.com>
2025-05-13 23:44:04 +0530

    Adding file couple_game.cpp

---
+++ codechef_100_900_/couple_game.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	
+	while (t--) {
+	    int b, g;
+	    cin >> g >> b;
+	    
+	    cout << b - g << endl;
+	}
+	
+	return 0;
+}
